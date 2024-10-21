a67cecb3b9983f02d567f485f4448ecb93adf944
yassinejsa2a <9872@holbertonstudents.com>
2024-10-21 15:11:19 +0200

    0

---
+++ hello_world/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    return (0);
+}
+julien@ubuntu:~/c/$ export CFILE=main.c
+julien@ubuntu:~/c/$ ./0-preprocessor 
+julien@ubuntu:~/c/$ tail c
