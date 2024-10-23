556dd0a3dedc827856f970c22eb7be98edf114c0
yassinejsa2a <9872@holbertonstudents.com>
2024-10-23 22:52:23 +0200

    6

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - tmp
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Taille de char : %zu octet(s)\n", sizeof(char));
+	printf("Taille de int : %zu octet(s)\n", sizeof(int));
+	printf("Taille de float : %zu octet(s)\n", sizeof(float));
+	printf("Taille de double : %zu octet(s)\n", sizeof(double));
+	printf("Taille de long : %zu octet(s)\n", sizeof(long));
+	printf("Taille de long long : %zu octet(s)\n", sizeof(long long));
+	return (0);
+}
