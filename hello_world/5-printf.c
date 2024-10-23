03b80e7e5a4368f2a1016aea515ce615e9725b13
yassinejsa2a <9872@holbertonstudents.com>
2024-10-23 22:33:19 +0200

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -8,4 +8,10 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
+	printf("     *     \n");
+    	printf("    ***    \n");
+    	printf("   *****   \n");
+    	printf("  *******  \n");
+    	printf(" ********* \n");
+    	printf("***********\n");
 }
