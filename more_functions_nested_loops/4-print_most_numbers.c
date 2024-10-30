1888f455a300d086a6a4c7fa8a4ca822cb31a17f
yassinejsa2a <9872@holbertonstudents.com>
2024-10-30 14:47:00 +0100

    4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -13,10 +13,10 @@ void print_most_numbers(void)
 
 	for (num = '0'; num <= '9'; num++)
 	{
-		if (num != 2 && num != 4)
-		{
-			_putchar(num);
-		}
+	if (num != '2' && num != '4')
+	{
+	_putchar(num);
+	}
 	}
 	_putchar('\n');
 }
