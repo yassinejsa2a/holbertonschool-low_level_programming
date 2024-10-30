d0926a458bf139c7be2ab97e61153b83661bf106
yassinejsa2a <9872@holbertonstudents.com>
2024-10-30 14:49:24 +0100

    4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,15 +8,14 @@
  */
 void print_most_numbers(void)
 {
-	int i;
 	char num;
 
-	for (num = '0'; num <= '9'; num++)
+		for (num = '0'; num <= '9'; num++)
 	{
-	if (num != '2' && num != '4')
+		if (num != '2' && num != '4')
 	{
-	_putchar(num);
+		_putchar(num);
 	}
 	}
-	_putchar('\n');
+		_putchar('\n');
 }
