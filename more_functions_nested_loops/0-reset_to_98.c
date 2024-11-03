b720f253539f7337094dd20f63f06010fda71af3
yassinejsa2a <9872@holbertonstudents.com>
2024-11-03 15:32:56 +0100

    0

---
+++ more_functions_nested_loops/0-reset_to_98.c
@@ -0,0 +1,12 @@
+include# "main.h"
+include# "stdio.h"
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ * @n: is a pointer to an integer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
