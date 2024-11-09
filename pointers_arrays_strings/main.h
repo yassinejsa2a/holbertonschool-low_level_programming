ebb6d45277e30fb1e95da5142b18a0ac81205c13
yassinejsa21 <9872@holbertonstudents.com>
2024-11-09 20:19:30 +0100

    4

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -54,3 +54,4 @@ char *leet(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
