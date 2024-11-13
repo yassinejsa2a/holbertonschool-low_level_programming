a82e0e6421a9d3ed37c7356f46f84944d6d9a5a6
yassinejsa21 <9872@holbertonstudents.com>
2024-11-13 17:34:45 +0100

    0

---
+++ malloc-free/0-create_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * create_array - function that creates an array of chars
+ *  and initializes it with a specific char
+ * @size: size of the car
+ * @c: char specified
+ * Return: array
+ */
+
+char *create_array(unsigned int size, char c);
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0, i < size, i++)
+	{
+		array[i] = c;
+	}
+
+	return (array);
+}
---
+++ malloc-free/main.c
@@ -0,0 +1,19 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *buffer;
+
+    buffer = create_array(98, 'H');
+    if  (buffer == NULL)
+    {
+        printf("failed to allocate memory\n");
+        return (1);
+    }
+    simple_print_buffer(buffer, 98);
+    free(buffer);
+    return (0);
+}
