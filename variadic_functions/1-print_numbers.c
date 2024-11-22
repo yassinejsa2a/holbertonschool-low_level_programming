e4a407b91a14c6225c2e5f8a8443061dbc130f6b
yassinejsa21 <9872@holbertonstudents.com>
2024-11-22 16:54:36 +0100

    1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -6,7 +6,6 @@
  * print_numbers - prints numbers, followed by a new line
  * @separator: string print nuber
  * @n: number passed function
- * @current_num: stock num 
  * @separator: separate
  */
 
@@ -14,18 +13,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i = 0;
-	int current_num;
 
 	va_start(ap, n);
 
 	while (i < n)
 	{
-		current_num = va_arg(ap, int);
-		printf("%d", current_num);
+		printf("%d", va_arg(ap, int));
 
 		if (separator && i < n - 1)
 		{
 			printf("%s", separator);
+
+			i++;
 		}
 
 	}
